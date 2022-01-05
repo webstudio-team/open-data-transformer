d7d89accd417810b28aeaa6c56e717fa7ab50bc7
panoskap <petr.panoska@uzis.cz>
2022-01-05 16:56:26 +0100

    T12534 [FEATURE] casting values by defined datatypes from config

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { parse } = require("csv-parse");
 const iconv = require("iconv-lite");
 const { transform } = require("stream-transform");
 const createCsvStringifier = require("csv-writer").createArrayCsvStringifier;
+const allowedDatatypes = ["string", "integer", "float", "boolean"];
 
 function getInputFilename() {
   const inputFilename = process.argv.slice(2)[0];
@@ -50,11 +51,45 @@ function readCsv(filename) {
     .pipe(parse(getParserConfig()));
 }
 
+function parseValue(value, datatype) {
+  if (!allowedDatatypes.includes(datatype)) {
+    throw new Error(`Datatype ${datatype} is not allowed or valid.`);
+  }
+
+  switch (datatype) {
+    case "string":
+      return value;
+    case "integer":
+      return parseInt(value);
+    case "float":
+      return parseFloat(value);
+    case "boolean":
+      return [1, "1", true, "true"].includes(value) ? 1 : 0;
+  }
+}
+
 const transformer = () => {
   const stringifier = createCsvStringifier(getStringifierConfig());
+  const datatypes = Object.values(config.columns).map(
+    (column) => column.datatype
+  );
+  let counter = 0;
 
   return transform((data) => {
-    return stringifier.stringifyRecords([data]);
+    counter++;
+    let parsedValues = [];
+
+    if (counter === 1) {
+      //prevent parsing headers
+      parsedValues = data;
+    } else {
+      data.forEach((value, index) => {
+        const datatype = datatypes[index];
+        parsedValues.push(parseValue(value, datatype));
+      });
+    }
+
+    return stringifier.stringifyRecords([parsedValues]);
   });
 };
 
