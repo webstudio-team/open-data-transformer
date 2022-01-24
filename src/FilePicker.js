f0b8baa14cb2ddd4bd26888b80595bfaf0e82562
vatehav <viliam.vateha@uzis.cz>
2022-01-24 13:54:31 +0100

    T12534 [REFACTOR] add delimiter and encoding options

--- src/FilePicker.js
+++ src/FilePicker.js
@@ -9,14 +9,18 @@ export default function FilePicker({ inputRef, handleFormChange }) {
       <label htmlFor="encoding">Kódování:</label>
       <div>
         <select onChange={handleFormChange} name="encoding" id="encoding">
+          <option value="">none</option>
           <option value="win1250">win1250</option>
+          <option value="utf8">utf8</option>
         </select>
       </div>
       <br />
       <label htmlFor="delimiter">Oddělovač:</label>
       <div>
         <select onChange={handleFormChange} name="delimiter" id="delimiter">
+          <option value="">none</option>
           <option value=";">;</option>
+          <option value=",">,</option>
         </select>
       </div>
       <div>
