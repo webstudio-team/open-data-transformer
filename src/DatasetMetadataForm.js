748dfcd061d97f0092e3800ab3d67864eada597a
vatehav <viliam.vateha@uzis.cz>
2022-03-10 15:55:36 +0100

    T12534 [REFACTOR] change dependencies array

--- src/DatasetMetadataForm.js
+++ src/DatasetMetadataForm.js
@@ -29,7 +29,7 @@ export default function DatasetMetadataForm({
         ),
       });
     },
-    [descriptionLengthLimit, setDatasetMetadata]
+    [descriptionLengthLimit, datasetMetadata]
   );
 
   return (
